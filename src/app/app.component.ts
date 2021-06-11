87891d3ac987d48a5db9103720a6e7868c80c02d
valeroMC <valerocomingsoon@gmail.com>
2021-06-11 08:45:23 -0500

    creating the calculator with angular forms and bootstrap.

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Mini calculadora';
+  numero1:number=0;
+  numero2:number=0;
+  resultado:number=0;
+  
+  sumar():void {
+   this.resultado = this.numero1 + this.numero2;
+  }
+  
+  restar():void {
+    this.resultado = this.numero1 - this.numero2;
+  
+  }
+  multiplicar():void{
+    this.resultado = this.numero1 * this.numero2;
+  }
+  dividir():void{
+    this.resultado = this.numero1 / this.numero2;
+  }
+
+} 
